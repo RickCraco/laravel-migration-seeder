a110c3320c58a08e1747eb66bcf92b5d7b034590
RickCraco <riccardocracolici43@gmail.com>
2023-12-21 14:50:33 +0100

    aggiunta funzione index al controller

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function index()
+    {
+        $trains = Train::all();
+        return view('trains.index', compact('trains'));
+    }
 }
